c4817352408026f03f7c3cda1c093bb53ce051f9
Joel <joelbenjamin093@gmail.com>
2018-02-12 22:45:37 +0530

    kinda done

--- __init__.py
+++ __init__.py
@@ -12,5 +12,4 @@ __version__     = '0.9.0i' # It's meant to be this way...
 
 # If it's meant to be, it'll be
 from .handler import Handler as Kemosabe
-from .payload_builder import payload
 from .wrapper_api import *
--- wrapper_api.py
+++ wrapper_api.py
@@ -1,16 +1,12 @@
 
 
 from .api import MessengerAPI
-from .configs import Configurations
-
-cfgs = Configurations()
-
-def set_configurations(api_token="",verify_tkn=""):
-    cfgs.set(access_token=api_token,verify_token=verify_tkn)
+from .payload_builder import payload
 
 
 token = "EAAExpe5DqoQBAILgztplgZBXj5IQrEj4wVpdItfR1LWGVZBC2MfJykoUPF25G92U4szjI96G1Gw6NfnrltgnQT2ShyW5PQgWZAKBYUHY6GlJ5OemZBbifLtMIVn1rObjNHs8IRpbEuXfZAXko5Rp6pg8GXbID1pN8n9TaZCkUo4AZDZD"
 bot = MessengerAPI(token)
+pload = payload()
 
 
 def send_text_message(uid,text):
@@ -24,3 +20,11 @@ def send_quick_reply(uid,text,payload):
 def send_card_templates(uid,payload):
     r = bot.generic_template(uid,payload)
     return r
+
+def create(action='',**kwargs):
+    pstr = pload.create(action,**kwargs)
+    return pstr
+
+def parse(action='',**kwargs):
+    pstr = pload.parse(action,**kwargs)
+    return pstr
