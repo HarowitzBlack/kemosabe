303f18c503d4f52a68f37088f017b18794ddc75f
Joel <joelbenjamin093@gmail.com>
2018-02-14 22:29:19 +0530

    a few changes

--- wrapper_api.py
+++ wrapper_api.py
@@ -11,10 +11,14 @@ pload = payload()
 
 
 def send_text_message(uid,text):
+    """Sends a text message to the user
+    """
     r = bot.send_text_message(uid,text)
     return r
 
 def send_quick_reply(uid,text,payload):
+    """Sends quick reply to the user
+    """
     r = bot.quick_reply(uid,text,payload)
     return r
 
@@ -23,16 +27,31 @@ def send_card_templates(uid,payload):
     return r
 
 def create(action='',**kwargs):
+    """ Builds the payload using the action and user-defined variables
+
+    >> create_payload(action="getallpetstores",pet="cat",color='white')
+
+    result:
+    >> @getallpetstores?&color=white&pet=cat
+
+    """
     pstr = pload.create(action,**kwargs)
     return pstr
 
 def parse(action='',**kwargs):
+    """
+    parses the payload string to extract params
+        >> @getallpetstores?&color=white&pet=cat
+
+        result:
+        >> {'color': 'white', 'pet': 'cat', 'action': '@getallpetstores'}
+    """
     pstr = pload.parse(action,**kwargs)
     return pstr
 
 def set_configurations(api_key="",path="."):
     """
-        
+        creates the config file to store api_key
     """
     if api_key == "" or len(api_key) < 177:
         print("Invalid configs")
